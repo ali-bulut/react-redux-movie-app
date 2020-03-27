210274c8e502b8ff0aae6c6dea594e52be49ae27
Ali Bulut <alibulut@yahoo.com>
2020-03-27 16:46:35 +0300

    new movie form validation operations

--- src/components/NewMovieForm.js
+++ src/components/NewMovieForm.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { Button, Form, Image } from "semantic-ui-react";
 
+import InlineError from './InlineError';
+
 class NewMovieForm extends Component {
     state={
         title:'',
-        cover:''
+        cover:'',
+        errors:{}
     }
 
     handleChange = (e) => {
@@ -13,17 +16,38 @@ class NewMovieForm extends Component {
         })
     }
 
+    onSubmit = () => {
+        const errors=this.validate();
+        this.setState({
+            errors:errors
+        });
+    }
+
+    validate = () => {
+        const errors={};
+        if(!this.state.title)
+            errors.title="Title cannot be blank!";
+        
+        if(!this.state.cover)
+            errors.cover="Cover Url cannot be blank!";
+        return errors;
+    }
+
   render() {
+      const errors=this.state.errors;
     return (
       <div>
           <h2>New Movie</h2>
-        <Form>
-          <Form.Field>
+        <Form onSubmit={this.onSubmit}>
+            {/* !! -> string ifadeyi boolean'a çevirir. */}
+          <Form.Field error={!!errors.title}>
             <label>Title</label>
+            {errors.title ? <InlineError message={errors.title}/> : null}
             <input id="title" name="title" value={this.state.title} onChange={this.handleChange} placeholder="Title" />
           </Form.Field>
-          <Form.Field>
+          <Form.Field error={!!errors.cover}>
             <label>Cover Url</label>
+            {errors.cover ? <InlineError message={errors.cover}/> : null}
             <input id="cover" name="cover" value={this.state.cover} onChange={this.handleChange} placeholder="Cover Url" />
           </Form.Field>
           <Form.Field>
